145d815ec240a69ac1f4d36be165de37a5827b72
Charith Bhat <charsept04@gmail.com>
2023-12-26 09:15:52 +0530

    another

--- app.js
+++ app.js
@@ -8,3 +8,42 @@ app.get('/',function(req, res) {
 app.use('/client',express.static(__dirname + '/client'));
  
 serv.listen(2000);
+console.log("server started");
+
+var SOCKET_LIST = {};
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+	socket.id = Math.random();
+	socket.x = 0;
+	socket.y = 0;
+	socket.number = "" + Math.floor(10 * Math.random());
+	SOCKET_LIST[socket.id] = socket;
+ 
+	socket.on('disconnect',function(){
+		delete SOCKET_LIST[socket.id];
+	});
+ 
+});
+
+setInterval(function(){
+	var pack = [];
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.x++;
+		socket.y++;
+		pack.push({
+			x:socket.x,
+			y:socket.y,
+			number:socket.number
+		});		
+	}
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPositions',pack);
+	}
+ 
+ 
+ 
+ 
+},1000/25);
